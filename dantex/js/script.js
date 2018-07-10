93dad58eb5c50960323883dd43108089acd59fef
Joanna Kowal <aska.szewczyk@gmail.com>
2018-07-10 21:46:59 +0200

    dantex rezygnacja

---
+++ dantex/js/script.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+$(document).ready(function () {
+    'use strict';
+    $("#btn-red").click(function () {
+        $("#content").addClass("remove");
+    });
+});
+=======
+$(document).ready(function(){
+    $("#btn-red").click(function(){
+        $("#content").addClass("remove");
+    });
+});
+
+>>>>>>> 9b6e6e22444d216a4336a84c8dfd18f664b096b8
